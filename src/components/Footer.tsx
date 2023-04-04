1a12b2952861a034540f297de74a2203a45d9966
NuminousBlue <jdiehl2236@gmail.com>
2023-04-04 16:21:27 -0500

    solid stopping point

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 
+const openInNewTab = (url: string) => window.open(url, "_blank");
+
 const Footer: React.FC<{}> = () => {
   return (
-    <footer
-      style={{ marginBottom: "0", fontSize: ".66rem", fontStyle: "bold" }}
-    >
-      <p
-        style={{
-          userSelect: "none",
-          cursor: "pointer",
-          width: "13rem",
-          margin: "0 auto"
-        }}
-      >
+    <footer style={{ cursor: "pointer" }}>
+      <p onClick={() => openInNewTab("https://github.com/JaynewDee")}>
         built by Joshua Newell Diehl
       </p>
-      <p>---</p>
-      <a
-        href="mailto:jdiehl2236@gmail.com"
-        style={{ color: "black", userSelect: "none" }}
-      >
-        EMAIL
-      </a>
     </footer>
   );
 };
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Tag = ({ data }: any) => {
+  // TODO
+  // On any tag click, add tag to results filter
+  // On click of active tag, remove from filter
+
   return (
     <div key={data}>
       <span className="tag">{data}</span>
